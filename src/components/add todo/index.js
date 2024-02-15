abdc0461b041f78a446b2cdc4a0c7556d424a7c0
Diana Dzakah <dianadzakah1@gmail.com>
2024-02-15 03:20:34 -0800

    set up my project

---
+++ src/components/add todo/index.js
@@ -0,0 +1,13 @@
+
+import styles from  './index.css';
+function AddTodo(){
+    return(
+        <section className='AddTodo'>
+            <input placeholder="start typing..."/> 
+            <button>Create</button>
+         </section>
+
+    );
+    }
+
+    export default AddTodo;
---
+++ src/components/todo list/index.js
@@ -0,0 +1,9 @@
+function TodoList(){
+    return(
+        <ul>
+        <li>complete javascript course</li> 
+        <li>jog around the park</li>
+        </ul>
+    );
+}
+export default TodoList;
